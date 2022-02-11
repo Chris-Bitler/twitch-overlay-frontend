e63caaa53f62573ef02dff6230dee08a6075c5b3
Chris-Bitler <chris@cbitler.io>
2022-02-11 09:54:03 -0800

    Switch to socketio

--- src/components/Overlay.jsx
+++ src/components/Overlay.jsx
@@ -1,41 +1,56 @@
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import '../css/Overlay.css';
-import useWebSocket from "react-use-websocket";
 import { convertEventToUserDisplay } from "../util/eventConverters";
 import {OverlayPart} from "./OverlayPart";
+import {io} from "socket.io-client";
 
 export const Overlay = () => {
     const params = useParams();
-    const { sendJsonMessage, lastMessage, readyState } = useWebSocket("wss://void-twitch-overlay.herokuapp.com/ws", {
-        shouldReconnect: () => true,
-        reconnectAttempts: 10,
-        reconnectInterval: 3000
-    });
     const [recentEvents, setRecentEvents] = useState([]);
-    useEffect(() => {
-        if (readyState === 1) {
-            const showUser = {
-                type: 'ADD_USER',
-                user: params.user
-            };
-            sendJsonMessage(showUser);
+
+    const addToRecentEvents = (event) => {
+        if (recentEvents.length >= 4) {
+            setRecentEvents([event, ...recentEvents.slice(0,3)]);
+        } else {
+            setRecentEvents([event, ...recentEvents]);
         }
-    }, [readyState]);
-
+    }
     useEffect(() => {
-       if (lastMessage != null) {
-           const eventMessage = convertEventToUserDisplay(JSON.parse(lastMessage?.data));
-           // @ts-ignore
-           eventMessage.key = new Date().getTime();
-           if (recentEvents.length >= 4) {
-               setRecentEvents([eventMessage, ...recentEvents.slice(0,3)]);
-           } else {
-               setRecentEvents([eventMessage, ...recentEvents]);
-           }
-       }
-    }, [lastMessage]);
-
+        const socket = io('https://void-twitch-overlay.herokuapp.com/socket.io', { query: `user=${params.user}`});
+        const key = new Date().getTime();
+        socket.on('connect', () => {
+            console.log('Connected to socketio server');
+        });
+        socket.on('disconnect', () => {
+            console.log('Disconnected from socketio server');
+        });
+        socket.on('sub', data => {
+            addToRecentEvents({ username: data.subber, text: 'Sub', key });
+        });
+        socket.on('raid', data => {
+            addToRecentEvents({ username: data.raider, text: `Raid (${data.viewers} viewers)`, key })
+        });
+        socket.on('cheer', data => {
+           addToRecentEvents({ username: data.cheerer, text: `${data.amount} bits`, key })
+        });
+        socket.on('follow', data => {
+            addToRecentEvents({ username: data.follower, text: 'Follow', key });
+        })
+        socket.on('gift_sub', data => {
+            let text = 'Gift x1';
+            if (data.giftAmount) {
+                text = `Gift x${data.giftAmount}`;
+            }
+            if (data.cumulativeGiftAmount) {
+                text += ` (${data.cumulativeGiftAmount})`;
+            }
+            addToRecentEvents({ username: data.gifter, text, key });
+        });
+        return () => {
+            socket.disconnect();
+        }
+    }, []);
 
     return <div>
         {recentEvents.map((event, index) => {
