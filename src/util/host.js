e5470ac70850a5021679914afedb134f95a5752c
Chris-Bitler <chris@cbitler.io>
2022-06-20 20:05:47 -0700

    Overlay dev

--- src/util/host.js
+++ src/util/host.js
@@ -1,2 +1,1 @@
-//export const socketIoHost = process.env.NODE_ENV === 'development' ? 'http://127.0.01' : 'https://twitch-overlay.voidwhisperer.info';
-export const socketIoHost = 'http://127.0.0.1';
+export const socketIoHost = 'https://twitch-overlay-dev.voidwhisperer.info';
